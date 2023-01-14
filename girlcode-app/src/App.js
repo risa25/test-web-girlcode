29c693ad5ddfbc9a4bcc92c45f1f4d2fe3c7e704
risa25 <108122480+risa25@users.noreply.github.com>
2023-01-14 14:54:14 -0800

    remove clothing

--- girlcode-app/src/App.js
+++ girlcode-app/src/App.js
@@ -1,18 +1,31 @@
 function changeDressImage() {
-  document.getElementById("model").src="modeldress.png";
+  if (document.getElementById("model").src.match("modeldress.png")) {
+    document.getElementById("model").src="modelresized.png";
+  } else {
+    document.getElementById("model").src="modeldress.png";
+  }
 }
 
 function changeShirtImage() {
   if (document.getElementById("model").src.match("modeljustskirt.png")) {
     document.getElementById("model").src="modelskirt.png";
-  } else {
+  }  else if (document.getElementById("model").src.match("modelshirt.png")) {
+    document.getElementById("model").src="modelresized.png";
+  } else if (document.getElementById("model").src.match("modelskirt.png")){
+    document.getElementById("model").src="modeljustskirt.png";
+  }else{
     document.getElementById("model").src="modelshirt.png";
   }
+  
 }
 
 function addSkirtImage() {
   if (document.getElementById("model").src.match("modeldress.png")||document.getElementById("model").src.match("modelresized.png"))  {
     document.getElementById("model").src="modeljustskirt.png";
+  } else if (document.getElementById("model").src.match("modeljustskirt.png")) {
+    document.getElementById("model").src="modelresized.png";
+  } else if (document.getElementById("model").src.match("modelskirt.png")) {
+    document.getElementById("model").src="modelshirt.png";
   } else {
     document.getElementById("model").src="modelskirt.png";
   }
