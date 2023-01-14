f226dbfd0c00dea3f77386e3af6ba881bd632fce
Linda Kiritchkov <118236865+lindakiritchkov@users.noreply.github.com>
2023-01-14 14:57:19 -0800

    can add and remove images properly

---
+++ src/App.js
@@ -0,0 +1,31 @@
+function changeDressImage() {
+  if (document.getElementById("model").src.match("modeldress.png")) {
+    document.getElementById("model").src = "modelresized.png";
+  } else {
+    document.getElementById("model").src = "modeldress.png";
+  }
+}
+
+function changeSkirtImage() {
+ if (document.getElementById("model").src.match("modelshirt.png")) {
+    document.getElementById("model").src = "modelfull.png";
+  } else if (document.getElementById("model").src.match("modelfull.png")) {
+   document.getElementById("model").src = "modelskirt.png";
+ } else if (document.getElementById("model").src.match("modelskirt.png")) {
+   document.getElementById("model").src = "modelresized.png";
+ } else {
+   document.getElementById("model").src = "modelskirt.png";
+ }
+}
+
+function changeShirtImage() {
+  if (document.getElementById("model").src.match("modelfull.png")) {
+    document.getElementById("model").src = "modelskirt.png";
+  } else if (document.getElementById("model").src.match("modelskirt.png")) {
+    document.getElementById("model").src = "modelfull.png";
+  } else if (document.getElementById("model").src.match("modelshirt.png")) {
+    document.getElementById("model").src = "modelresized.png";
+  } else {
+    document.getElementById("model").src = "modelshirt.png";
+  }
+}
