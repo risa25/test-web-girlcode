ada59a7577010462cd90d1b1e4d967d5dd7eeea6
Linda Kiritchkov <118236865+lindakiritchkov@users.noreply.github.com>
2023-01-14 14:56:41 -0800

    add and remove functionality added

--- girlcode-app/src/App.js
+++ girlcode-app/src/App.js
@@ -1,33 +1,31 @@
 function changeDressImage() {
   if (document.getElementById("model").src.match("modeldress.png")) {
-    document.getElementById("model").src="modelresized.png";
+    document.getElementById("model").src = "modelresized.png";
   } else {
-    document.getElementById("model").src="modeldress.png";
+    document.getElementById("model").src = "modeldress.png";
   }
+}
+
+function changeSkirtImage() {
+ if (document.getElementById("model").src.match("modelshirt.png")) {
+    document.getElementById("model").src = "modelfull.png";
+  } else if (document.getElementById("model").src.match("modelfull.png")) {
+   document.getElementById("model").src = "modelskirt.png";
+ } else if (document.getElementById("model").src.match("modelskirt.png")) {
+   document.getElementById("model").src = "modelresized.png";
+ } else {
+   document.getElementById("model").src = "modelskirt.png";
+ }
 }
 
 function changeShirtImage() {
-  if (document.getElementById("model").src.match("modeljustskirt.png")) {
-    document.getElementById("model").src="modelskirt.png";
-  }  else if (document.getElementById("model").src.match("modelshirt.png")) {
-    document.getElementById("model").src="modelresized.png";
-  } else if (document.getElementById("model").src.match("modelskirt.png")){
-    document.getElementById("model").src="modeljustskirt.png";
-  }else{
-    document.getElementById("model").src="modelshirt.png";
-  }
-  
-}
-
-function addSkirtImage() {
-  if (document.getElementById("model").src.match("modeldress.png")||document.getElementById("model").src.match("modelresized.png"))  {
-    document.getElementById("model").src="modeljustskirt.png";
-  } else if (document.getElementById("model").src.match("modeljustskirt.png")) {
-    document.getElementById("model").src="modelresized.png";
+  if (document.getElementById("model").src.match("modelfull.png")) {
+    document.getElementById("model").src = "modelskirt.png";
   } else if (document.getElementById("model").src.match("modelskirt.png")) {
-    document.getElementById("model").src="modelshirt.png";
+    document.getElementById("model").src = "modelfull.png";
+  } else if (document.getElementById("model").src.match("modelshirt.png")) {
+    document.getElementById("model").src = "modelresized.png";
   } else {
-    document.getElementById("model").src="modelskirt.png";
+    document.getElementById("model").src = "modelshirt.png";
   }
-
 }
